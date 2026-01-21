308e72bb9589d8397d879933074552b48ebbdfe2
Cole <564603061@qq.com>
2026-01-21 11:35:01 +0800

    优化构建脚本

--- publish_linux.ps1
+++ publish_linux.ps1
@@ -1,1 +1,3 @@
-dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=true /p:IncludeNativeLibrariesForSelfExtract=true
+# 本项目 在windows上交叉编译linux程序，需要依赖zig
+# 我们后续可以手动在linux 通过strip裁剪调试符号
+dotnet publish -c Release -r linux-x64 -p:PublishAot=true -p:StripSymbols=false
